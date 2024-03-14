b2fc37cb4fe76d23ea7c96b1f76f6774f4294daf
Anton Bushuiev <anton.bushuiev@gmail.com>
2024-03-14 15:13:34 +0100

    Refactor mutation class

--- mutils/mutations.py
+++ mutils/mutations.py
@@ -2,6 +2,9 @@ import itertools
 import copy
 import warnings
 import math
+from typing import Union
+from dataclasses import dataclass
+from pathlib import Path
 
 import Bio.PDB
 from Bio.PDB.Polypeptide import protein_letters_3to1
@@ -14,122 +17,192 @@ from mutils.misc import verbose
 protein_letters_1to3 = {v: k for k, v in protein_letters_3to1.items()}
 
 
-class Mutation:
-    def __init__(self, mutstr='', muttuple=()):
-        """
-        :param mut: String-represented single or multi-point mutation.
-            Example of the string for a double point mutation: YC17T,TA20A.
-            Here, point mutations are separated with comma and in the form
-            <Wild-type residue><Chain id><Residue number><Mutated residue>
-        """
-        # TODO Check format
+@dataclass
+class PointMutation:
+    wt: str = None
+    chain: str = None
+    pos: int = None
+    ins: str = None
+    m: str = None
 
-        # Check datatypes
-        if not isinstance(muttuple, tuple):
-            try:
-                muttuple = tuple(muttuple)
-            except:
-                raise ValueError('Wrong `muttuple` datatype')
-
-        # Init both representations
-        # TODO Make only single representation based on `wt`, `mut`, `pos`, `chain` and `ins`
-        # attributes
-        if mutstr != '' and muttuple != ():
-            raise ValueError('Wrong constructor argument values.')
-        elif mutstr != '':
-            self.muttuple = tuple(mutstr.replace(' ', '').split(','))
+    @classmethod
+    def from_str(cls, pmut: str, check_amino_acid_codes: bool = True) -> 'PointMutation':
+        if not len(pmut):
+            return None, None, None, None
+        
+        first_digit = [x.isdigit() for x in pmut].index(True)
+        contains_insertion = pmut[-2].isalpha()
+        if contains_insertion:
+            wt, chain, pos, ins, m = \
+                pmut[0], pmut[1:first_digit], int(pmut[first_digit:-2]), pmut[-2], pmut[-1]
         else:
-            self.muttuple = muttuple
+            wt, chain, pos, ins, m = \
+                pmut[0], pmut[1:first_digit], int(pmut[first_digit:-1]), None, pmut[-1]
+            
+        if check_amino_acid_codes:
+            assert wt in AMINO_ACID_CODES_1, f'Wrong wild-type amino acid code: {wt}'
+            assert m in AMINO_ACID_CODES_1, f'Wrong mutant amino acid code: {m}'
 
-    def revert(self):
-        """
-        Example: YC17T -> TC17Y
-        :return: Reversed mutation
-        """
-        return Mutation(muttuple=tuple(map(self.revert_single, self.muttuple)))
+        return cls(wt, chain, pos, ins, m)
+
+    def revert(self) -> 'PointMutation':
+        return PointMutation(self.m, self.chain, self.pos, self.ins, self.wt)
 
     @verbose
-    def is_mutation_reversed(self, pdbfile, model_id=0):
-        """
-        Check if mutation is reversed or direct with respect to the PDB file.
-        :param pdbfile: PDB file corresponding to current Mutation instance
-        :param model_id: Model id in PDB file
-        :return: Bool
-        """
-        # Select first mutation
-        # (Relaxed to check only the first one for better performance)
-        mut = self.muttuple[0]
-
-        # Parse mutation info
-        wtres, chainid, pos, mutres = mut[0], mut[1], int(mut[2:-1]), mut[-1]
+    def wt_in_pdb(self, pdb_path: Union[Path, str], model_id: int = 0) -> bool:
+        pdb_path = Path(pdb_path)
 
         # Read structure
         warnings.simplefilter('ignore')
-        model = Bio.PDB.PDBParser().get_structure(None, pdbfile)[model_id]
+        model = Bio.PDB.PDBParser().get_structure(None, pdb_path)[model_id]
 
         # Check mutated amino acid
-        resname = model[chainid][pos].get_resname()
-        resname = Bio.PDB.Polypeptide.three_to_one(resname)
-        if resname == wtres:
+        try:
+            resname = model[self.chain][self.pos].get_resname()
+        except KeyError:
             return False
-        elif resname == mutres:
-            return True
-        else:
-            raise ValueError(f'{mut} is neither forward nor reverse.')
+        resname = Bio.PDB.Polypeptide.three_to_one(resname)
+        return resname == self.wt
 
-    def rename_chains(self, dct):
-        """
-        :param dct: Rename dict. For example {'C': 'A'} to replace chaind 'C'
-            for 'A'
-        :return: Mutation with renamed chains
-        """
-        return Mutation(muttuple=tuple(map(
-            lambda mut: self.rename_chains_single(mut, dct), self.muttuple
-        )))
+
+    def __str__(self) -> str:
+        return f'{self.wt}{self.chain}{self.pos}{self.ins}{self.m}'
+
+
+@dataclass
+class Mutation:
+    muts: list = None
+
+    @classmethod
+    def from_str(cls, mut: str, check_amino_acid_codes: bool = True) -> 'Mutation':
+        return cls([PointMutation.from_str(m, check_amino_acid_codes) for m in mut.split(',')])
     
-    def insertion(self):
-        """
-        :return: True if mutation is insertion, False otherwise for each point
-        """
-        ret = [m[-1].isalpha() and m[-2].isalpha() for m in self.muttuple]
-        return ret
+    def revert(self) -> 'Mutation':
+        return Mutation([m.revert() for m in self.muts])
     
-    def wt_to_graphein(self):
-        """
-        Example: 'YC17T,AC17AG' -> ['CYS:Y:17','CYS:A:17:A']
-        :return: Wild types in graphein format
-        """
-        ret = []
-        for m, ins in zip(self.muttuple, self.insertion()):
-            if ins:
-                parts = [protein_letters_1to3[m[0]], m[1], m[2:-2], m[-2]]
-            else:
-                parts = [protein_letters_1to3[m[0]], m[1], m[2:-1]]
-            ret.append(':'.join(parts))
-        return ret
+    def __str__(self) -> str:
+        return ','.join([str(m) for m in self.muts])
+    
+    def wt_in_pdb(self, pdb_path: Union[Path, str], model_id: int = 0) -> bool:
+        return all([m.wt_in_pdb(pdb_path, model_id) for m in self.muts])
 
-    @staticmethod
-    def parse_single(mut):
-        if len(mut) == 0:
-            return None, None, None, None
-        # TODO Not general enough
-        # Chain may be missing, chain may be more than 1 character
-        else:
-            return mut[0], mut[1], int(mut[2:-1]), mut[-1]
 
-    @staticmethod
-    def revert_single(mut):
-        return mut[-1] + mut[1:-1] + mut[0]
+# class Mutation:
+#     def __init__(self, mutstr='', muttuple=()):
+#         """
+#         :param mut: String-represented single or multi-point mutation.
+#             Example of the string for a double point mutation: YC17T,TA20A.
+#             Here, point mutations are separated with comma and in the form
+#             <Wild-type residue><Chain id><Residue number><Mutated residue>
+#         """
+#         # TODO Check format
 
-    @staticmethod
-    def rename_chains_single(mut, dct):
-        return mut[:1] + dct[mut[1]] + mut[2:]
+#         # Check datatypes
+#         if not isinstance(muttuple, tuple):
+#             try:
+#                 muttuple = tuple(muttuple)
+#             except:
+#                 raise ValueError('Wrong `muttuple` datatype')
 
-    def __str__(self):
-        return ','.join(self.muttuple)
+#         # Init both representations
+#         # TODO Make only single representation based on `wt`, `mut`, `pos`, `chain` and `ins`
+#         # attributes
+#         if mutstr != '' and muttuple != ():
+#             raise ValueError('Wrong constructor argument values.')
+#         elif mutstr != '':
+#             self.muttuple = tuple(mutstr.replace(' ', '').split(','))
+#         else:
+#             self.muttuple = muttuple
 
-    def __repr__(self):
-        return f'Mutation({str(self)})'
+#     def revert(self):
+#         """
+#         Example: YC17T -> TC17Y
+#         :return: Reversed mutation
+#         """
+#         return Mutation(muttuple=tuple(map(self.revert_single, self.muttuple)))
+
+#     @verbose
+#     def is_mutation_reversed(self, pdbfile, model_id=0):
+#         """
+#         Check if mutation is reversed or direct with respect to the PDB file.
+#         :param pdbfile: PDB file corresponding to current Mutation instance
+#         :param model_id: Model id in PDB file
+#         :return: Bool
+#         """
+#         # Select first mutation
+#         # (Relaxed to check only the first one for better performance)
+#         mut = self.muttuple[0]
+
+#         # Parse mutation info
+#         wtres, chainid, pos, mutres = mut[0], mut[1], int(mut[2:-1]), mut[-1]
+
+#         # Read structure
+#         warnings.simplefilter('ignore')
+#         model = Bio.PDB.PDBParser().get_structure(None, pdbfile)[model_id]
+
+#         # Check mutated amino acid
+#         resname = model[chainid][pos].get_resname()
+#         resname = Bio.PDB.Polypeptide.three_to_one(resname)
+#         if resname == wtres:
+#             return False
+#         elif resname == mutres:
+#             return True
+#         else:
+#             raise ValueError(f'{mut} is neither forward nor reverse.')
+
+#     def rename_chains(self, dct):
+#         """
+#         :param dct: Rename dict. For example {'C': 'A'} to replace chaind 'C'
+#             for 'A'
+#         :return: Mutation with renamed chains
+#         """
+#         return Mutation(muttuple=tuple(map(
+#             lambda mut: self.rename_chains_single(mut, dct), self.muttuple
+#         )))
+    
+#     def insertion(self):
+#         """
+#         :return: True if mutation is insertion, False otherwise for each point
+#         """
+#         ret = [m[-1].isalpha() and m[-2].isalpha() for m in self.muttuple]
+#         return ret
+    
+#     def wt_to_graphein(self):
+#         """
+#         Example: 'YC17T,AC17AG' -> ['CYS:Y:17','CYS:A:17:A']
+#         :return: Wild types in graphein format
+#         """
+#         ret = []
+#         for m, ins in zip(self.muttuple, self.insertion()):
+#             if ins:
+#                 parts = [protein_letters_1to3[m[0]], m[1], m[2:-2], m[-2]]
+#             else:
+#                 parts = [protein_letters_1to3[m[0]], m[1], m[2:-1]]
+#             ret.append(':'.join(parts))
+#         return ret
+
+#     @staticmethod
+#     def parse_single(mut):
+#         if len(mut) == 0:
+#             return None, None, None, None
+#         # TODO Not general enough
+#         # Chain may be missing, chain may be more than 1 character
+#         else:
+#             return mut[0], mut[1], int(mut[2:-1]), mut[-1]
+
+#     @staticmethod
+#     def revert_single(mut):
+#         return mut[-1] + mut[1:-1] + mut[0]
+
+#     @staticmethod
+#     def rename_chains_single(mut, dct):
+#         return mut[:1] + dct[mut[1]] + mut[2:]
+
+#     def __str__(self):
+#         return ','.join(self.muttuple)
+
+#     def __repr__(self):
+#         return f'Mutation({str(self)})'
 
 
 class MutationSpace:
